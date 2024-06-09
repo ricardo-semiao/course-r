aeb051ffe9a7cc9dc7a575cdd987c3a3490f454a
ricardo-semiao <ricardo.semiao@outlook.com>
2024-06-08 23:32:34 -0300

    Created book template and added navbar from personal homepage

---
+++ site_structure.js
@@ -0,0 +1,21 @@
+fetch('site_structure.html')
+    .then(response => response.text())
+    .then(data => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+
+        const headerHTML = doc.querySelector('#site-header').outerHTML;
+        const footerHTML = doc.querySelector('#site-footer').outerHTML;
+
+        document.querySelector('#site-header-container').innerHTML = headerHTML;
+        document.querySelector('#site-footer-container').innerHTML = footerHTML;
+    });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.getElementsByClassName('site-nav-link');
+    for (var i = 0; i < navLinks.length; i++) {
+        if (navLinks[i].href == document.URL) {
+            navLinks[i].classList.add('current-page');
+        }
+    }
+});
